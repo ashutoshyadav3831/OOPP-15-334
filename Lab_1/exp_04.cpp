0427f00e2109bad4ac418936ded8c96736fdc598
ashawinanand-hash <ashawinanand@gmail.com>
2026-09-18 08:56:41 +0530

    Lab 1

---
+++ Lab_1/exp_04.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Time
+{
+    int hh, mm, ss;
+
+public:
+    void input(int h, int m, int s)
+    {
+        hh = h;
+        mm = m;
+        ss = s;
+    }
+    void show()
+    {
+        cout << hh << ":" << mm << ":" << ss << endl;
+    }
+};
+
+int main()
+{
+    Time t1, t2;
+    t1.input(10, 25, 30);
+    t2.input(15, 40, 50);
+
+    t1.show();
+    t2.show();
+}
